310984ef083179e04923c45b44f448bdd069140b
Naseef AR <naseef14@gmail.com>
2016-10-27 02:15:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,18 +119,20 @@ app.get('/articles/:articleID', function (req, res) {
 // get the list of all available articles
 
 app.get('/articles', function (req,res) {
-    pool.query("SELECT heading FROM articles", function (err, result){
-        if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            if (results.rows.length === 0) {
-                res.send("<h3 alighn='center'>No articles in the DB</h3>");
-            } else {
-                // res.send(makeTemplateForArticleList(result.rows));
-                res.send("list here");
-            }
-        }
-    });
+    // pool.query("SELECT heading FROM articles", function (err, result){
+    //     if (err) {
+    //         res.status(500).send(err.toString());
+    //     } else {
+    //         if (results.rows.length === 0) {
+    //             res.send("<h3 alighn='center'>No articles in the DB</h3>");
+    //         } else {
+    //             // res.send(makeTemplateForArticleList(result.rows));
+    //             res.send("list here");
+    //         }
+    //     }
+    // });
+    
+    res.send("list of articles..");
 });
 
 
