e7f6a7ba3fe5ff51a0d2a33322cdcca17dfbe632
Naseef AR <naseef14@gmail.com>
2016-10-26 13:52:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/articles/:articleID', function (req, res) {
         res.status(500).send(err.toString());
     } else {
         if (result.rows.length === 0) {
-            res.status(404).send('Article not found');
+            res.status(404).send('Boola... Article not found');
         } else {
             var articleData = result.rows[0];
             res.send(createTemplate(articleData));
